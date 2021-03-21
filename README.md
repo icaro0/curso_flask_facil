2a658cd08b439d46913d1525311cfe617765773d
Ramon Guerrero <icaro0@gmail.com>
2021-03-21 20:32:46 +0100

    Subida inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Curso de páginas web con python
+
+## Primeros pasos
+
+Instalar el entorno con pip install -r requeriments.txt
+
+export FLASK_APP=app.py
+
+export FLASK_ENV=development
+
+flask run
---
+++ app.py
@@ -0,0 +1,10 @@
+#imports
+from flask import Flask
+from flask import render_template
+
+#declaramos nuestra app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
